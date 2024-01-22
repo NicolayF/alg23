29214e7503bfc212a2cb2b661a6edd4a5aa8c3c4
linuxbox <linuxbox@linuxbox.myguest.virtualbox.org>
2024-01-22 17:54:17 -0300

    lab02 ej3

--- lab02/ej3/sort.c
+++ lab02/ej3/sort.c
@@ -16,11 +16,34 @@ static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
      - elements in a[izq,pivot) all 'go_before' (according to function goes_before) a[pivot]
      - a[pivot] 'goes_before' all the elements in a(pivot,der]
     */
+    unsigned int i = izq + 1;
+    unsigned int j = der;
+    unsigned int ppiv = izq;
+
+    while (i <= j) {
+        if (goes_before(a[i], a[ppiv])) {
+            i++;
+        }
+        else if (goes_before(a[ppiv], a[j])) {
+            j--;
+        }
+        else if (goes_before(a[ppiv],a[i]) && goes_before(a[j], a[ppiv])) {
+            swap(a, i, j);
+        }
+    }
+    swap(a, ppiv, j);
+    ppiv = j;
+    return ppiv;
 }
 
 static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
     /* copiá acá la implementación que hiciste en el ejercicio 3 */
-
+    unsigned int ppiv;
+    if (der > izq) {
+        ppiv = partition(a, izq, der);
+        quick_sort_rec(a, izq, ppiv);
+        quick_sort_rec(a, ppiv+1, der);
+    }
 }
 
 void quick_sort(int a[], unsigned int length) {
