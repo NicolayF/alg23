3144633a28608419c8b018828727f5b78f2f4cc8
linuxbox <linuxbox@linuxbox.myguest.virtualbox.org>
2024-01-26 20:54:47 -0300

    lab04 ej3a

---
+++ lab04/ej3/list.h
@@ -0,0 +1,35 @@
+#ifndef _LIST_H
+#define _LIST_H
+
+#include <stdbool.h>
+
+typedef int list_elem;
+typedef struct _list * list;
+
+list empty();
+
+void addl(list l, list_elem e);
+
+bool is_empty(list l);
+
+list_elem head(list l);
+
+void tail(list l);
+
+void addr(list l, list_elem e);
+
+unsigned int length(list l);
+
+void concat(list l, list l0);
+
+list_elem index(list l, unsigned int n);
+
+void take(list l, unsigned int n);
+
+void drop(list l, unsigned int n);
+
+list copy_list(list l);
+
+void destroy(list l);
+
+#endif
