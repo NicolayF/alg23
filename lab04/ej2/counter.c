ac99cedc621bd529c7eb1a5450d538367de96b52
linuxbox <linuxbox@linuxbox.myguest.virtualbox.org>
2024-01-26 16:50:09 -0300

    lab04 ej2a

--- lab04/ej2/counter.c
+++ lab04/ej2/counter.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "counter.h"
 
@@ -8,37 +9,31 @@ struct _counter {
 };
 
 counter counter_init(void) {
-/*
-    Needs implementation.
-*/
+    counter c = malloc(sizeof(counter));
+    c->count = 0;
+    return c;
 }
 
 void counter_inc(counter c) {
-/*
-    Needs implementation.
-*/
+    c->count++;
 }
 
 bool counter_is_init(counter c) {
-/*
-    Needs implementation.
-*/
+    return c->count == 0;
 }
 
 void counter_dec(counter c) {
-/*
-    Needs implementation.
-*/
+    assert(!counter_is_init(c));
+    c->count--;
 }
 
 counter counter_copy(counter c) {
-/*
-    Needs implementation.
-*/
+    counter c_copy = malloc(sizeof(counter));
+    c_copy->count = c->count;
+    return c_copy;
 }
 
 void counter_destroy(counter c) {
-/*
-   Needs implementation.
-*/
+    free(c);
+    c = NULL;
 }
