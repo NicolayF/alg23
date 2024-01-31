2c1a25beed240072559077e69a7399322cc2cd2d
linuxbox <linuxbox@linuxbox.myguest.virtualbox.org>
2024-01-30 22:55:25 -0300

    lab05 ej2

--- lab05/ej2/stack.c
+++ lab05/ej2/stack.c
@@ -8,3 +8,49 @@ struct _s_stack {
     unsigned int capacity;  // Capacidad actual del arreglo elems
 };
 
+stack stack_empty() {
+    stack s = malloc(sizeof(struct _s_stack));
+    s->elems = NULL;
+    s->size = 0;
+    s->capacity = 1;
+    return s;
+}
+
+stack stack_push(stack s, stack_elem e) {
+    if (s->size == s->capacity) {
+        s->capacity *= 2;
+        s->elems = realloc(s->elems, sizeof(stack_elem) * s->capacity);
+    }
+    s->elems[s->size] = e;
+    s->size++;
+    return s;
+}
+
+stack stack_pop(stack s) {
+    assert(s->size > 0);
+    s->size--;
+    return s;
+}
+
+unsigned int stack_size(stack s) {
+    return s->size;
+}
+
+stack_elem stack_top(stack s) {
+    assert(s->size > 0);
+    return s->elems[s->size - 1];
+}
+
+bool stack_is_empty(stack s) {
+    return s->size == 0;
+}
+
+stack_elem *stack_to_array(stack s) {
+    return s->elems;
+}
+
+stack stack_destroy(stack s) {
+    free(s->elems);
+    free(s);
+    return NULL;
+}
